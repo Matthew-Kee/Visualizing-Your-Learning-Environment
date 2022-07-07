45f76cfe195e06ef288edf83adc1c615fc115b8b
Matthew Kee <therealmattkee@gmail.com>
2022-07-07 01:28:43 -0400

    Added callbacks for weekday buttons to change the graph when clicked.

--- interactive-app.py
+++ interactive-app.py
@@ -1,4 +1,5 @@
-# Hi Professor! Ignore the setup steps below. You can view the prototype hosted online at [ADD LINK HERE]. Thanks!
+# Hi Professor! Ignore the setup steps below. 
+# You can view the prototype hosted online at [ADD LINK HERE]. Thanks!
 
 # Setup (for devs):
 # pip install dash
@@ -8,8 +9,8 @@
 # TODO:
 # Functionality we NEED before any visual changes:
 # - Add callbacks so that the Monday-Thursday buttons/dropdown 
-# - Add data for different locations and different days (can fake this, for proof-of-concept can use straight lines)
-# - Figure out how to do the line segments for different locations (honestly if this is too time consuming we can rethink it)
+# - Add data for different locations and different days
+# - Figure out how to do the line segments for different locations - done!
 
 # Visual nice-to-haves:
 # Add images to each dropdown option
@@ -18,8 +19,9 @@
 # import pandas for Data Frame manipulation
 import pandas as pd
 
-# import Dash class, the dcc (Dash Core Components) module, Dash html, Output and Input (for callbacks), and the dbc (Dash Bootstrap Components) module
-from dash import Dash, dcc, html, Output, Input  
+# import Dash class, the dcc (Dash Core Components) module, Dash html, callback context, 
+# Output and Input (for callbacks), and the dbc (Dash Bootstrap Components) module
+from dash import Dash, dcc, html, callback_context, Output, Input  
 import dash_bootstrap_components as dbc
 
 # import Plotly express module, Plotly graph objects, and the make_subplots function
@@ -46,12 +48,39 @@ app = Dash(__name__, external_stylesheets=[dbc.themes.JOURNAL])
 title = dcc.Markdown(children='Epic Title')
 subtitle = dcc.Markdown(children='Visualizing daily student experience in place and time.')
 week = dcc.Markdown(children='July 4th to July 7th')
-mondayButton = html.Button('Monday', id='monday-button', style=white_button_style)
-tuesdayButton = html.Button('Tuesday', id='tuesday-button', style=white_button_style)
-wednesdayButton = html.Button('Wednesday', id='wednesday-button', style=white_button_style)
-thursdayButton = html.Button('Thursday', id='thursday-button', style=white_button_style)
+mondayButton = html.Button('Monday', id='monday-button', n_clicks=0, style=white_button_style)
+tuesdayButton = html.Button('Tuesday', id='tuesday-button', n_clicks=0, style=white_button_style)
+wednesdayButton = html.Button('Wednesday', id='wednesday-button', n_clicks=0, style=white_button_style)
+thursdayButton = html.Button('Thursday', id='thursday-button', n_clicks=0, style=white_button_style)
 summaryTitle = dcc.Markdown(children='Check out other weeks:')
-graph = dcc.Graph(figure={}) 
+graph = dcc.Graph(id='graph', figure={})
+
+# # CALLBACKS FOR BUTTONS:
+@app.callback(
+    # `component_id`, `component_property`
+    Output(component_id='graph', component_property='figure'),
+    Input(component_id='monday-button', component_property='n_clicks'),
+    Input(component_id='tuesday-button', component_property='n_clicks'),
+    Input(component_id='wednesday-button', component_property='n_clicks'),
+    Input(component_id='thursday-button', component_property='n_clicks'))
+def update_figure(monday, tuesday, wednesday, thursday):
+    button_id = [p['prop_id'] for p in callback_context.triggered][0]
+    if 'monday-button' in button_id:
+        figure = fig.update
+        figure = make_subplots(rows=3, cols=1, subplot_titles=("Monday", "Monday", "Monday"))
+        return figure
+    elif 'tuesday-button' in button_id:
+        figure = make_subplots(rows=3, cols=1, subplot_titles=("Tuesday", "Tuesday", "Tuesday"))
+        return figure
+    elif 'wednesday-button' in button_id:
+        figure = make_subplots(rows=3, cols=1, subplot_titles=("Wednesday", "Wednesday", "Wednesday"))
+        return figure
+    elif 'thursday-button' in button_id:
+        figure = make_subplots(rows=3, cols=1, subplot_titles=("Thursday", "Thursday", "Thursday"))
+        return figure
+    else:
+        figure = make_subplots(rows=3, cols=1, subplot_titles=("Default", "Default", "Default"))
+        return figure
 
 # MAKE FIGURES:
 # make 3 rows of subplots, for Temperature, Humidity, and Light
