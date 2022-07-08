77ec51f5128ee84269bd71565f37b7e42cc9b5ff
Matthew Kee <therealmattkee@gmail.com>
2022-07-07 23:04:22 -0400

    Reformatted x-axes to show readable values. Added different legends for each subplot. Resolved issue where gaps of data were being interpolated instead of left empty. Wrote the createDefaultPlot function to create the Temperature, Humidity and Light plots for Monday and plot data from each location as a different trace. Also added csv of cumulative data and created a small TestData csv for testing.

--- interactive-app.py
+++ interactive-app.py
@@ -8,9 +8,10 @@
 
 # TODO:
 # Functionality we NEED before any visual changes:
-# - Add callbacks so that the Monday-Thursday buttons/dropdown 
-# - Add data for different locations and different days
+# - Add callbacks so that the Monday-Thursday buttons/dropdown - done!
 # - Figure out how to do the line segments for different locations - done!
+# - Write a default function that sets up the graph for the first time and is only called once - done!
+# - Write a function that updates the graph for data from each specific day when buttons are clicked
 
 # Visual nice-to-haves:
 # Add images to each dropdown option
@@ -18,6 +19,9 @@
 
 # import pandas for Data Frame manipulation
 import pandas as pd
+
+# import NumPy for number manipulation
+from numpy import NaN
 
 # import Dash class, the dcc (Dash Core Components) module, Dash html, callback context, 
 # Output and Input (for callbacks), and the dbc (Dash Bootstrap Components) module
@@ -30,32 +34,53 @@ import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 
 # STYLES:
-white_button_style = {'background-color': 'white',
+whiteButtonStyle = {'background-color': 'white',
                       'color': 'black',
                       'height': '50px',
                       'width': '400px'}
 
+# CONSTANTS:
+MONDAY_CLASS_START_TIME_1 = pd.to_datetime('2022-07-04 08:00:00')
+MONDAY_CLASS_END_TIME_1 = pd.to_datetime('2022-07-04 11:50:00')
+MONDAY_LOUNGE_START_TIME = pd.to_datetime('2022-07-04 11:50:00')
+MONDAY_LOUNGE_END_TIME = pd.to_datetime('2022-07-04 13:00:00')
+MONDAY_CLASS_START_TIME_2 = pd.to_datetime('2022-07-04 13:00:00')
+MONDAY_CLASS_END_TIME_2 = pd.to_datetime('2022-07-04 15:20:00')
+MONDAY_DC_START_TIME = pd.to_datetime('2022-07-04 15:20:00')
+MONDAY_DC_END_TIME = pd.to_datetime('2022-07-04 23:00:00')
+
+FIGURE_HEIGHT = 1000
+
 # PARSE CSV FILE:
 # read the csv of mock data from the file hosted on GitHub and import it into a df (data frame) which is a table of rows and columns
-csvFile = 'https://raw.githubusercontent.com/malakali542/361Design1/main/mock_data1.csv'
+# csvFile = 'https://raw.githubusercontent.com/malakali542/361Design1/main/mock_data1.csv'
+csvFile = 'Data_July4ToJuly6.csv'
+# csvFile = 'TestData.csv'
 df = px.pd.read_csv(csvFile)
-# TODO: STOP HARDCODING THE DATE, I am hardcoding to June 30th 2022 right now because that's when the mock data was taken
-# append the date of data collection to the times in the "Time" column so that Pandas can interpret the string as a DateTime object
-df["Time"] = pd.to_datetime("2022-06-30" + ' ' + df["Time"])
+# use the Date and Time columns to create a DateTime column of DateTime objects
+df.insert(loc=0, column='DateTime', value=pd.to_datetime(df['Date'] + ' ' + df['Time'], format='%d-%m-%Y %H:%M:%S'), allow_duplicates=True)
 
 # DECLARE COMPONENTS:
 app = Dash(__name__, external_stylesheets=[dbc.themes.JOURNAL])
 title = dcc.Markdown(children='Epic Title')
 subtitle = dcc.Markdown(children='Visualizing daily student experience in place and time.')
 week = dcc.Markdown(children='July 4th to July 7th')
-mondayButton = html.Button('Monday', id='monday-button', n_clicks=0, style=white_button_style)
-tuesdayButton = html.Button('Tuesday', id='tuesday-button', n_clicks=0, style=white_button_style)
-wednesdayButton = html.Button('Wednesday', id='wednesday-button', n_clicks=0, style=white_button_style)
-thursdayButton = html.Button('Thursday', id='thursday-button', n_clicks=0, style=white_button_style)
+mondayButton = html.Button('Monday', id='monday-button', n_clicks=0, style=whiteButtonStyle)
+tuesdayButton = html.Button('Tuesday', id='tuesday-button', n_clicks=0, style=whiteButtonStyle)
+wednesdayButton = html.Button('Wednesday', id='wednesday-button', n_clicks=0, style=whiteButtonStyle)
+thursdayButton = html.Button('Thursday', id='thursday-button', n_clicks=0, style=whiteButtonStyle)
 summaryTitle = dcc.Markdown(children='Check out other weeks:')
 graph = dcc.Graph(id='graph', figure={})
 
-# # CALLBACKS FOR BUTTONS:
+fig = make_subplots(rows=3, cols=1, subplot_titles=('Temperature', 'Humidity', 'Light'))
+fig.update_layout(
+    height = FIGURE_HEIGHT,
+    # yaxis_range=[20,30],
+    showlegend=True,
+    legend_tracegroupgap=(FIGURE_HEIGHT-100)/3)    
+fig.update_xaxes(tickformat="%I:%M %p")
+
+# CALLBACKS FOR BUTTONS:
 @app.callback(
     # `component_id`, `component_property`
     Output(component_id='graph', component_property='figure'),
@@ -66,71 +91,66 @@ graph = dcc.Graph(id='graph', figure={})
 def update_figure(monday, tuesday, wednesday, thursday):
     button_id = [p['prop_id'] for p in callback_context.triggered][0]
     if 'monday-button' in button_id:
-        figure = fig.update
-        figure = make_subplots(rows=3, cols=1, subplot_titles=("Monday", "Monday", "Monday"))
+        figure = make_subplots(rows=3, cols=1, subplot_titles=('Monday', 'Monday', 'Monday'))
         return figure
     elif 'tuesday-button' in button_id:
-        figure = make_subplots(rows=3, cols=1, subplot_titles=("Tuesday", "Tuesday", "Tuesday"))
+        figure = make_subplots(rows=3, cols=1, subplot_titles=('Tuesday', 'Tuesday', 'Tuesday'))
         return figure
     elif 'wednesday-button' in button_id:
-        figure = make_subplots(rows=3, cols=1, subplot_titles=("Wednesday", "Wednesday", "Wednesday"))
+        figure = make_subplots(rows=3, cols=1, subplot_titles=('Wednesday', 'Wednesday', 'Wednesday'))
         return figure
     elif 'thursday-button' in button_id:
-        figure = make_subplots(rows=3, cols=1, subplot_titles=("Thursday", "Thursday", "Thursday"))
+        figure = make_subplots(rows=3, cols=1, subplot_titles=('Thursday', 'Thursday', 'Thursday'))
         return figure
     else:
-        figure = make_subplots(rows=3, cols=1, subplot_titles=("Default", "Default", "Default"))
-        return figure
+        print("HI MATTHEW")
+        return createDefaultPlot()
+        
 
-# MAKE FIGURES:
-# make 3 rows of subplots, for Temperature, Humidity, and Light
-fig = make_subplots(rows=3, cols=1, subplot_titles=("Temperature", "Humidity", "Light"))
-fig.update_layout(height = 1000)
+def createDefaultPlot():
+    figure = fig
+    figure.data = []
 
-# add section 1 of the Temperature vs. Time plot in row 1
-startTime1 = pd.to_datetime("2022-06-30 13:00:00")
-endTime1 = pd.to_datetime("2022-06-30 13:54:00")
-fig.add_trace((go.Scatter(
-    x=df.loc[(df["Time"] > startTime1) & (df["Time"] < endTime1), "Time"],
-    y=df.loc[(df["Time"] > startTime1) & (df["Time"] < endTime1), " Temperature"],
-    mode="lines",
-    name="E7 temperature",
-)), row=1, col=1)
+    # array of the 3 columns of data in the csv that we will plot against time
+    plots = ['Temperature', 'Humidity', 'Light']
+    # enumerate through all of the subplots
+    for index, plot in enumerate(plots, start=1):
+        # Add E7 6008 section
+        figure.add_trace((go.Scatter(
+            x=df.loc[(df['DateTime'] > MONDAY_CLASS_START_TIME_1) & (df['DateTime'] < MONDAY_CLASS_END_TIME_2), 'DateTime'],
+            y=df.loc[(df['DateTime'] > MONDAY_CLASS_START_TIME_1) & (df['DateTime'] < MONDAY_CLASS_END_TIME_1) | 
+            (df['DateTime'] > MONDAY_CLASS_START_TIME_2) & (df['DateTime'] < MONDAY_CLASS_END_TIME_2), plot],
+            mode='lines',
+            name='E7 6008',
+            connectgaps=False,
+            line=dict(color='#FF2D2D'),
+            legendgroup=index,
+            # hovertemplate='%{x}<extra></extra>'
+        )), row=index, col=1)
 
-# add section 2 of the Temperature vs. Time plot in row 1
-startTime2 = endTime1
-endTime2 = pd.to_datetime("2022-06-30 14:20:00")
-fig.add_trace((go.Scatter(
-    x=df.loc[(df["Time"] > startTime2) & (df["Time"] < endTime2), "Time"],
-    y=df.loc[(df["Time"] > startTime2) & (df["Time"] < endTime2), " Temperature"],
-    mode="lines",
-    name="SYDE lounge temperature",
-)), row=1, col=1)
+        # Add SYDE lounge section
+        figure.add_trace((go.Scatter(
+            x=df.loc[(df['DateTime'] > MONDAY_LOUNGE_START_TIME) & (df['DateTime'] < MONDAY_LOUNGE_END_TIME), 'DateTime'],
+            y=df.loc[(df['DateTime'] > MONDAY_LOUNGE_START_TIME) & (df['DateTime'] < MONDAY_LOUNGE_END_TIME), plot],
+            mode='lines',
+            name='SYDE lounge',
+            connectgaps=False,
+            line=dict(color='#5048E5'),
+            legendgroup=index,
+        )), row=index, col=1)
 
-# add section 3 of the Temperature vs. Time plot in row 1
-startTime3 = endTime2
-endTime3 = pd.to_datetime("2022-06-30 15:00:00")
-fig.add_trace((go.Scatter(
-    x=df.loc[(df["Time"] > startTime3) & (df["Time"] < endTime3), "Time"],
-    y=df.loc[(df["Time"] > startTime3) & (df["Time"] < endTime3), " Temperature"],
-    mode="lines",
-    name="DC library temperature",
-)), row=1, col=1)
+        # Add DC section
+        figure.add_trace((go.Scatter(
+            x=df.loc[(df['DateTime'] > MONDAY_DC_START_TIME) & (df['DateTime'] < MONDAY_DC_END_TIME), 'DateTime'],
+            y=df.loc[(df['DateTime'] > MONDAY_DC_START_TIME) & (df['DateTime'] < MONDAY_DC_END_TIME), plot],
+            mode='lines',
+            name='SYDE lounge',
+            connectgaps=False,
+            line=dict(color='#5048E5'),
+            legendgroup=index,
+        )), row=index, col=1)
 
-# add the Humidity vs. Time plot in row 2
-fig.add_trace((go.Scatter(
-    x=df["Time"],
-    y=df[" humidity"],
-    mode="lines",
-    name="humidity"
-)), row=2, col=1)
-# add the Light vs. Time plot in row 3
-fig.add_trace((go.Scatter(
-    x=df["Time"],
-    y=df[" Light"],
-    mode="lines",
-    name="light"
-)), row=3, col=1)
+    return figure
 
 # LAYOUT COMPONENTS USING GRIDS:
 # declare a top-level container
